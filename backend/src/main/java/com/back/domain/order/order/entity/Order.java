eea89a4a320c1782bba32b04b6ee6a7935d37a05
dooongdaeng <snake10102@naver.com>
2025-07-14 16:23:44 +0900

    ãstandard 위치 변경

---
+++ backend/src/main/java/com/back/domain/order/order/entity/Order.java
@@ -0,0 +1,15 @@
+package com.back.domain.order.order.entity;
+
+
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Order extends BaseEntity {
+
+
+}
