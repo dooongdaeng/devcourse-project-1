68894cd668b08c2d74958555b08eb8211827a030
heigangbae <j56233145@gmail.com>
2025-07-15 09:28:08 +0900

    feat : create Rq class for session

---
+++ backend/src/main/java/com/back/global/rq/Rq.java
@@ -0,0 +1,32 @@
+package com.back.global.rq;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@RequiredArgsConstructor
+public class Rq {
+    private final HttpServletRequest req;
+    private final HttpServletResponse res;
+
+    public Integer getLoginedUserId() {
+        return (Integer) req.getSession().getAttribute("loginedUserId");
+    }
+
+    public boolean isLogined() {
+        return getLoginedUserId() != null;
+    }
+
+    public void login(int userId) {
+        req.getSession().setAttribute("loginedUserId", userId);
+    }
+
+    public void logout() {
+        req.getSession().invalidate();
+    }
+}
