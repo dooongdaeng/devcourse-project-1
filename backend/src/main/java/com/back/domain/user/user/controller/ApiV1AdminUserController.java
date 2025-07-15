d46c80fc30aeb4a8a710a2627d1fb95531be0446
heigangbae <j56233145@gmail.com>
2025-07-15 11:25:38 +0900

    refactor : rename ApiV1adminUserController

--- backend/src/main/java/com/back/domain/user/user/controller/ApiV1AdminUserController.java
+++ backend/src/main/java/com/back/domain/user/user/controller/ApiV1AdminUserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/adm/users")
 @RequiredArgsConstructor
-public class ApiV1AdminUserController {
+public class ApiV1AdminUserController{
     private final UserService userService;
 
     @GetMapping
