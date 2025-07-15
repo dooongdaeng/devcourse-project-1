1a5339088052305699d97242327a62ea15d80204
eunchan96 <taegwon02@gmail.com>
2025-07-15 14:36:41 +0900

    feat: ProductImageDto

---
+++ backend/src/main/java/com/back/domain/product/productImage/dto/ProductImageDto.java
@@ -0,0 +1,23 @@
+package com.back.domain.product.productImage.dto;
+
+import com.back.domain.product.productImage.entity.ProductImage;
+
+import java.time.LocalDateTime;
+
+public record ProductImageDto(
+        int id,
+        LocalDateTime createDate,
+        LocalDateTime ModifyDate,
+        String url,
+        int productId
+){
+    public ProductImageDto(ProductImage productImage) {
+        this(
+                productImage.getId(),
+                productImage.getCreateDate(),
+                productImage.getModifyDate(),
+                productImage.getUrl(),
+                productImage.getProduct().getId()
+        );
+    }
+}
