50736614375ef0ce47702fd0744d212e3e2711f8
eunchan <taegwon02@gmail.com>
2025-07-15 15:22:43 +0900

    feat: Product 조회 (#96)
    * feat: Product 다건조회
    
    * feat: Product 단건조회
    
    * feattesroduct 다건조회 - 404
    
    * fix: 오류 수정

--- backend/src/test/java/com/back/domain/product/product/controller/ApiV1ProductControllerTest.java
+++ backend/src/test/java/com/back/domain/product/product/controller/ApiV1ProductControllerTest.java
@@ -1,7 +1,7 @@
 package com.back.domain.product.product.controller;
 
-import com.back.domain.product.product.service.ProductService;
 import com.back.domain.product.product.entity.Product;
+import com.back.domain.product.product.service.ProductService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +14,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -60,5 +63,80 @@ public class ApiV1ProductControllerTest {
                 .andExpect(jsonPath("$.data.price").value(product.getPrice()))
                 .andExpect(jsonPath("$.data.description").value(product.getDescription()))
                 .andExpect(jsonPath("$.data.stock").value(product.getStock()));
+    }
+
+    @Test
+    @DisplayName("상품 다건 조회")
+    public void t2() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/products")
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print());
+
+        List<Product> products = productService.findAll();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1ProductController.class))
+                .andExpect(handler().methodName("getItems"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(products.size()));
+
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i);
+            resultActions
+                    .andExpect(jsonPath("$[%d].id".formatted(i)).value(product.getId()))
+                    .andExpect(jsonPath("$[%d].createDate".formatted(i)).value(Matchers.startsWith(product.getCreateDate().toString().substring(0, 20))))
+                    .andExpect(jsonPath("$[%d].modifyDate".formatted(i)).value(Matchers.startsWith(product.getModifyDate().toString().substring(0, 20))))
+                    .andExpect(jsonPath("$[%d].name".formatted(i)).value(product.getName()))
+                    .andExpect(jsonPath("$[%d].price".formatted(i)).value(product.getPrice()))
+                    .andExpect(jsonPath("$[%d].description".formatted(i)).value(product.getDescription()))
+                    .andExpect(jsonPath("$[%d].stock".formatted(i)).value(product.getStock()));
+        }
+    }
+
+    @Test
+    @DisplayName("상품 단건 조회")
+    public void t3() throws Exception {
+        int id = 1;
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/products/%d".formatted(id))
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print());
+
+        Product product = productService.findById(id).get();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1ProductController.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(product.getId()))
+                .andExpect(jsonPath("$.createDate").value(Matchers.startsWith(product.getCreateDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.modifyDate").value(Matchers.startsWith(product.getModifyDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.name").value(product.getName()))
+                .andExpect(jsonPath("$.price").value(product.getPrice()))
+                .andExpect(jsonPath("$.description").value(product.getDescription()))
+                .andExpect(jsonPath("$.stock").value(product.getStock()));
+    }
+
+    @Test
+    @DisplayName("상품 단건 조회 - 404")
+    public void t4() throws Exception {
+        int id = Integer.MAX_VALUE;
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/products/%d".formatted(id))
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1ProductController.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.resultCode").value("404-1"))
+                .andExpect(jsonPath("$.msg").value("해당 데이터가 존재하지 않습니다."));
     }
 }
