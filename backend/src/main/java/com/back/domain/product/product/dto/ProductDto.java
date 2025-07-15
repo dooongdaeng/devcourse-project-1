7bc48e040cbb89543a80ed5a7988bcecd90d06e3
eunchan <taegwon02@gmail.com>
2025-07-15 14:38:06 +0900

    feat: ProductDto (#87)
    * feat: ProductDto
    
    * feat: ProductImageDto

---
+++ backend/src/main/java/com/back/domain/product/product/dto/ProductDto.java
@@ -0,0 +1,26 @@
+package com.back.domain.product.product.dto;
+
+import com.back.domain.product.product.entity.Product;
+
+import java.time.LocalDateTime;
+
+public record ProductDto (
+        int id,
+        LocalDateTime createDate,
+        LocalDateTime ModifyDate,
+        String name,
+        int price,
+        String description,
+        int stock
+){
+    public ProductDto(Product product) {
+        this(product.getId(),
+                product.getCreateDate(),
+                product.getModifyDate(),
+                product.getName(),
+                product.getPrice(),
+                product.getDescription(),
+                product.getStock()
+        );
+    }
+}
