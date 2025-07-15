8b29285a4ff5208ce579f1c09bbd7a9f6cf8215a
dooongdaeng <snake10102@naver.com>
2025-07-15 10:46:12 +0900

    test

--- backend/src/main/java/com/back/domain/order/order/controller/ApiV1OrderController.java
+++ backend/src/main/java/com/back/domain/order/order/controller/ApiV1OrderController.java
@@ -2,18 +2,19 @@ package com.back.domain.order.order.controller;
 
 
 import com.back.domain.order.order.dto.OrderDto;
+import com.back.domain.order.order.entity.Order;
 import com.back.domain.order.order.service.OrderService;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -23,11 +24,40 @@ public class ApiV1OrderController {
     private final OrderService orderService;
     private final Rq rq;
 
-    @GetMapping
-    @Transactional(readOnly = true)
-    @Operation(summary = "주문 목록 조회")
-    public RsData<List<OrderDto>> getItems() {
+    record OrderCreateReqBody(
+            int orderCount,
+            int totalPrice,
+            String paymentMethod
+    ) {
+    }
 
+    @PostMapping
+    @Transactional
+    @Operation(summary = "주문 생성")
+    public RsData<OrderDto> create(
+            @Valid @RequestBody OrderCreateReqBody reqBody
+    ) {
+        if (!rq.isLogined()){
+            return new RsData<>(
+                    "401-1",
+                    "로그인 후 이용해주세요."
+            );
+        }
+
+        int userId = rq.getLoginedUserId();
+
+        Order order = orderService.write(
+                userId,
+                reqBody.orderCount(),
+                reqBody.totalPrice(),
+                reqBody.paymentMethod()
+        );
+
+        return new RsData<>(
+                "201-1",
+                "주문이 생성되었습니다.",
+                new OrderDto(order)
+        );
     }
 
 }
--- backend/src/main/java/com/back/domain/order/order/service/OrderService.java
+++ backend/src/main/java/com/back/domain/order/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.back.domain.order.order.service;
 
+import com.back.domain.order.order.entity.Order;
 import com.back.domain.order.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,6 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
 
-
+    public Order write(int userId, int i, int i1, String s) {
+    }
 }
