3a51bc9439096ad3c4ea0514ac35c5273e69c1b9
heigangbae <113930804+HeegangBae@users.noreply.github.com>
2025-07-16 12:40:53 +0900

    feat: 세션 기반 인증을 JWT 쿠키 기반 인증으로 전환 (#153)
    * refactor : session 인증 관련 삭제
    
    * refactor : UserController 에서 session 인증 기반 로그인 로그아웃 에서 jwt 쿠키 인증 기반으로 변경
    
    * pref : securityConfig 에서 스프링 시큐리티가 세션을 사용하지 않도록 STATELESS 정책을 설정

--- backend/src/main/java/com/back/global/security/SecurityConfig.java
+++ backend/src/main/java/com/back/global/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -40,7 +41,9 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-//                .sessionManagement(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(
                         exceptionHandling -> exceptionHandling
