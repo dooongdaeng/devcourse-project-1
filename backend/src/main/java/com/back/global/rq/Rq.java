9196b73eb14d390103f8cd33223435330cae8cc0
heigangbae <j56233145@gmail.com>
2025-07-16 11:06:35 +0900

    pref : add jwt cookie method in Rq class

--- backend/src/main/java/com/back/global/rq/Rq.java
+++ backend/src/main/java/com/back/global/rq/Rq.java
@@ -1,11 +1,14 @@
 package com.back.global.rq;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
@@ -37,4 +40,35 @@ public class Rq {
         }
         return loginedUserId;
     }
+
+    public String getCookieValue(String name, String defaultValue) {
+        if (req.getCookies() == null) return defaultValue;
+        return Arrays.stream(req.getCookies())
+                .filter(cookie -> cookie.getName().equals(name))
+                .map(Cookie::getValue)
+                .filter(value -> !value.isBlank())
+                .findFirst()
+                .orElse(defaultValue);
+    }
+
+    public void setCookie(String name, String value) {
+        if (value == null) value = "";
+
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setDomain("localhost");  // 필요시 도메인 환경변수 처리 권장
+        cookie.setSecure(true);
+        cookie.setAttribute("SameSite", "Strict");
+
+        if (value.isBlank()) cookie.setMaxAge(0);
+        else cookie.setMaxAge(60 * 60 * 24 * 365);
+
+        res.addCookie(cookie);
+    }
+
+    public void deleteCookie(String name) {
+        setCookie(name, null);
+    }
+
 }
