656722fda683f225b8e46d3a6b7e50064a4aaa05
heigangbae <113930804+HeegangBae@users.noreply.github.com>
2025-07-16 10:00:57 +0900

    feat : UserAuthTokenService 도입 (#135)
    * feat : create UserAuthTokenService
    
    * pref : payload 에서 필요한 정보만 넘기게 변경

---
+++ backend/src/main/java/com/back/domain/user/user/service/UserAuthTokenService.java
@@ -0,0 +1,54 @@
+package com.back.domain.user.user.service;
+
+import com.back.domain.user.user.entity.User;
+import com.back.standard.util.Ut;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class UserAuthTokenService{
+    @Value("${custom.jwt.secretKey}")
+    private String jwtSecretKey;
+
+    @Value("${custom.jwt.expireSeconds}")
+    private int expireSeconds;
+
+    public String genAccessToken(User user) {
+        return Ut.jwt.toString(
+                jwtSecretKey,
+                expireSeconds,
+                Map.of(
+                        "id", user.getId(),
+                        "username", user.getUsername(),
+                        "nickname", user.getNickname(),
+                        "role", user.getRole()
+                )
+        );
+    }
+
+    public Map<String, Object> payload(String accessToken) {
+        Map<String, Object> parsedPayload = Ut.jwt.payload(jwtSecretKey, accessToken);
+
+        if (parsedPayload == null) return null;
+
+        try {
+            int id = (int) parsedPayload.get("id");
+            String username = (String) parsedPayload.get("username");
+            String nickname = (String) parsedPayload.get("nickname");
+            String role = (String) parsedPayload.get("role");
+
+            return Map.of(
+                    "id", id,
+                    "username", username,
+                    "nickname", nickname,
+                    "role", role
+            );
+        } catch (Exception e) {
+            return null; // 잘못된 payload 처리
+        }
+    }
+}
