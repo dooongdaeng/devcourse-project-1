71aba59d9746daf031642678692d34d112ffe717
johnbosco0414 <johnbosco0414@gmail.com>
2025-07-16 12:33:39 +0900

    refactor: 메인페이지 ui개선

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -18,9 +18,7 @@ export default function Home() {
                 <img src={product.imageUrl} alt={product.name} className="mb-3 h-40 w-full object-cover"/>
                 <h2 className="mb-3 text-2xl font-semibold">
                   {product.name}{" "}
-                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
+                  
                 </h2>
                 <p className="m-0 max-w-[30ch] text-sm opacity-50">
                   {product.description}
