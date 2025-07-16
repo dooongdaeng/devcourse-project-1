e8a6c5ab6b6fd7b81eb4aa19e20b9fbc9cb76e6d
heigangbae <j56233145@gmail.com>
2025-07-16 12:10:06 +0900

    pref : adminController에 swagger 적용

--- backend/src/main/java/com/back/domain/user/user/controller/ApiV1AdminUserController.java
+++ backend/src/main/java/com/back/domain/user/user/controller/ApiV1AdminUserController.java
@@ -3,6 +3,9 @@ package com.back.domain.user.user.controller;
 import com.back.domain.user.user.dto.AdminUserDto;
 import com.back.domain.user.user.entity.User;
 import com.back.domain.user.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +17,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/adm/users")
 @RequiredArgsConstructor
+@Tag(name = "관리자 유저 API")
+@SecurityRequirement(name = "bearerAuth")
 public class ApiV1AdminUserController {
     private final UserService userService;
 
     @GetMapping
+    @Operation(summary = "모든 사용자 정보 조회 (관리자)")
     public List<AdminUserDto> getUsers() {
         return userService.findAll().stream()
                 .map(AdminUserDto::new)
@@ -25,6 +31,7 @@ public class ApiV1AdminUserController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "특정 사용자 정보 조회 (관리자)")
     public AdminUserDto getUser(@PathVariable int id) {
         User user = userService.findById(id)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 회원입니다."));
