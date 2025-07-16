6e37e4224fb041e4ded4388251b988278841aa69
johnbosco0414 <johnbosco0414@gmail.com>
2025-07-16 14:45:35 +0900

    feat: mainPage wishlist button c

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { useProducts } from "@/context/ProductContext";
-import Link from "next/link";
+import { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
 
 export default function Home() {
   const { products } = useProducts();
+  const [favoriteProducts, setFavoriteProducts] = useState<{ [key: string]: boolean }>({});
+
+  const toggleFavorite = (productId: string) => {
+    setFavoriteProducts(prevFavorites => ({
+      ...prevFavorites,
+      [productId]: !prevFavorites[productId]
+    }));
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
@@ -14,16 +24,18 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         {products.map((product) => (
-            <Link key={product.id} href={`/products/${product.id}`} passHref className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <div key={product.id} className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
                 <img src={product.imageUrl} alt={product.name} className="mb-3 h-40 w-full object-cover"/>
-                <h2 className="mb-3 text-2xl font-semibold">
+                <h2 className="mb-3 text-xl font-semibold">
                   {product.name}{" "}
-                  
+                  <button onClick={(e) => { e.stopPropagation(); toggleFavorite(product.id); }} className="cursor-pointer">
+                    {favoriteProducts[product.id] ? <FaHeart color="red" /> : <FaRegHeart />}
+                  </button>
                 </h2>
                 <p className="m-0 max-w-[30ch] text-sm opacity-50">
                   {product.description}
                 </p>
-            </Link>
+            </div>
           ))}
       </div>
     </main>
