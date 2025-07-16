7bca3baf98f1911a0aa24d5be66ee123b4d8c52b
heigangbae <113930804+HeegangBae@users.noreply.github.com>
2025-07-16 09:33:02 +0900

    pref : add apikey & findByApiKey (#133)

--- backend/src/main/java/com/back/domain/user/user/repository/UserRepository.java
+++ backend/src/main/java/com/back/domain/user/user/repository/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    Optional<User> findByApiKey(String apiKey);
 }
