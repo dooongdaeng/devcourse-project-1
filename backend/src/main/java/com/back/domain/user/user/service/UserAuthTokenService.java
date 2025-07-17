a166c81acb389282acec03434c7f20b7ee5dd965
heigangbae <j56233145@gmail.com>
2025-07-17 11:00:59 +0900

    feat : UserAuthTokenService 에 genRefreshToken 및 payloadRefreshToken 메서드를 추가

--- backend/src/main/java/com/back/domain/user/user/service/UserAuthTokenService.java
+++ backend/src/main/java/com/back/domain/user/user/service/UserAuthTokenService.java
@@ -17,10 +17,26 @@ public class UserAuthTokenService{
     @Value("${custom.accessToken.expirationSeconds}")
     private int expireSeconds;
 
+    @Value("${custom.refreshToken.expirationSeconds}")
+    private int refreshTokenExpireSeconds;
+
     public String genAccessToken(User user) {
         return Ut.jwt.toString(
                 jwtSecretKey,
                 expireSeconds,
+                Map.of(
+                        "id", user.getId(),
+                        "username", user.getUsername(),
+                        "nickname", user.getNickname(),
+                        "role", user.getRole()
+                )
+        );
+    }
+
+    public String genRefreshToken(User user) {
+        return Ut.jwt.toString(
+                jwtSecretKey,
+                refreshTokenExpireSeconds, // 리프레시 토큰 만료 시간 사용
                 Map.of(
                         "id", user.getId(),
                         "username", user.getUsername(),
@@ -49,6 +65,26 @@ public class UserAuthTokenService{
             );
         } catch (Exception e) {
             return null; // 잘못된 payload 처리
+        }
+    }
+
+    public Map<String, Object> payloadRefreshToken(String refreshToken) {
+        Map<String, Object> parsedPayload = Ut.jwt.payload(jwtSecretKey, refreshToken);
+        if (parsedPayload == null) return null;
+        try {
+            int id = (int) parsedPayload.get("id");
+            String username = (String) parsedPayload.get("username");
+            String nickname = (String) parsedPayload.get("nickname");
+            String role = (String) parsedPayload.get("role");
+
+            return Map.of(
+                    "id", id,
+                    "username", username,
+                    "nickname", nickname,
+                    "role", role
+            );
+        } catch (Exception e) {
+            return null;
         }
     }
 }
