54f9a6b9912dbf6a4d38c32e38729cc69e3356d5
eunchan96 <taegwon02@gmail.com>
2025-07-18 17:37:10 +0900

    style: decription 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
     title: "Grids & Circles",
-    description: "Grids & Circles",
+    description: "Grids & Circles는 다양한 커피 메뉴를 손쉽게 주문하고 즐길 수 있는 스마트한 커피 주문 플랫폼입니다. 오늘도 당신의 커피 한 잔을 책임질게요!",
 };
 
 export default function RootLayout({
