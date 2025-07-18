0f35eb73165a9fcd9fa952ac0e0f467a398d4ca6
heigangbae <113930804+HeegangBae@users.noreply.github.com>
2025-07-18 14:16:57 +0900

    pref : 로그인 과 회원가입 페이지에 apiFetch 적용 (#282)
    * pref : login 페이지 apiFetch 적용
    
    * pref : signup 페이지 apiFetch 적용

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true, // 개발 모드에서 두 번 렌더링하여 잠재적인 문제를 찾는데 도움을 줍니다.
+    async rewrites() {
+        return [
+            {
+                source: '/api/v1/:path*', // /api/v1/로 시작하는 모든 요청
+                destination: 'http://localhost:8080/api/v1/:path*', // 실제 백엔드 API 서버 주소
+            },
+        ];
+    },
 };
 
 export default nextConfig;
