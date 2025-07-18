d346ffa78f1be6e20a631e439c02c7389a7ab694
eunchan <taegwon02@gmail.com>
2025-07-18 11:14:18 +0900

    feat: 회원관리 페이지에 fetch 적용 (#269)
    * fix: ref 수정
    
    * feat: fetch 적용
    
    * feat: AdminUserDto에 주소, 우편번호 추가

--- frontend/src/app/account/admin/accountManagement/page.tsx
+++ frontend/src/app/account/admin/accountManagement/page.tsx
@@ -1,16 +1,59 @@
 "use client";
 
-import { useState, ChangeEvent } from 'react';
-import { useProducts, User } from '@/context/ProductContext';
+import { useState, useEffect } from 'react';
+import { useProducts } from '@/context/ProductContext';
+import type { components } from '@/lib/backend/apiV1/schema';
+import { apiFetch } from '@/lib/backend/client';
+
+type User = components['schemas']['AdminUserDto'];
+
+const useUsers = () => {
+  const [ users, setUsers ] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    apiFetch('/api/v1/adm/users')
+      .then(setUsers)
+      .catch((error) => {
+        alert(`${error.resultCode} : ${error.msg}`);
+      });
+  }, []);
+
+  const deleteUser = (id: number, onSuccess: (data: any) => void) => {
+    apiFetch(`/api/v1/adm/users/${id}`, {
+      method: "DELETE"
+    }).then(onSuccess)
+    .catch((error) => {
+      alert(`${error.resultCode} : ${error.msg}`);
+    });
+  };
+
+  return { users, setUsers, deleteUser };
+}
 
 export default function AccountManagement() {
-  const { users, deleteUser } = useProducts();
+  const { users, setUsers, deleteUser } = useUsers();
 
   const handleDeleteUser = (userId: number) => {
     if (window.confirm('정말로 이 회원을 삭제하시겠습니까?')) {
-      deleteUser(userId);
+      deleteUser(userId, (res) => {
+        alert(res.msg);
+        if(users == null) return;
+        setUsers(users.filter((user) => user.id ! == userId));
+      });
     }
   };
+
+  const formatDate = (isoDate: string) => {
+    const _isoDate = isoDate.split('.')[0];
+    const [year, month, dayWithTime] = _isoDate.split('-');
+    const [day, time] = dayWithTime.split('T');
+    const [hour, min] = time.split(':');
+    return `${year}.${month}.${day} ${hour}:${min}`;
+  };
+
+  if(users == null) {
+    return <div>회원정보가 없습니다.</div>
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center p-4 md:p-24">
@@ -21,28 +64,31 @@ export default function AccountManagement() {
           <table className="min-w-full bg-white border border-gray-200 rounded-md">
             <thead>
               <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
-                <th className="py-3 px-6 text-left">ID</th>
-                <th className="py-3 px-6 text-left">사용자 ID</th>
-                <th className="py-3 px-6 text-left">이름</th>
-                <th className="py-3 px-6 text-left">우편번호</th>
-                <th className="py-3 px-6 text-left">주소</th>
-                <th className="py-3 px-6 text-left">가입일</th>
-                <th className="py-3 px-6 text-left">역할</th>
+                <th className="py-3 px-4 text-center">ID</th>
+                <th className="py-3 px-6 text-center">사용자 ID</th>
+                <th className="py-3 px-6 text-center">이름</th>
+                <th className="py-3 px-6 text-center">우편번호</th>
+                <th className="py-3 px-6 text-center">주소</th>
+                <th className="py-3 px-6 text-center">가입일</th>
+                <th className="py-3 px-6 text-center">역할</th>
                 <th className="py-3 px-6 text-center">액션</th>
               </tr>
             </thead>
             <tbody className="text-gray-600 text-sm">
               {users.map(user => (
                 <tr key={user.id} className="border-b border-gray-200 hover:bg-gray-50">
-                  <td className="py-3 px-6 text-left whitespace-nowrap">{user.id}</td>
-                  <td className="py-3 px-6 text-left">{user.userId}</td>
-                  <td className="py-3 px-6 text-left">{user.name}</td>
-                  <td className="py-3 px-6 text-left">{user.postalCode}</td>
-                  <td className="py-3 px-6 text-left">{user.address}</td>
-                  <td className="py-3 px-6 text-left">{user.signupDate}</td>
-                  <td className="py-3 px-6 text-left">{user.role}</td>
+                  <td className="py-3 px-4 text-center whitespace-nowrap">{user.id}</td>
+                  <td className="py-3 px-6 text-center">{user.username}</td>
+                  <td className="py-3 px-6 text-center">{user.nickname}</td>
+                  <td className="py-3 px-6 text-center">{user.postalCode}</td>
+                  <td className="py-3 px-6 text-center">{user.address}</td>
+                  <td className="py-3 px-6 text-center">{(user.createDate) ? formatDate(user.createDate) : ""}</td>
+                  <td className="py-3 px-6 text-center">{user.role?.replace(/^ROLE_/, '')}</td>
                   <td className="py-3 px-6 text-center">
-                    <button onClick={() => handleDeleteUser(user.id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded text-xs cursor-pointer">삭제</button>
+                    <button onClick={() => handleDeleteUser((user.id !== undefined) ? user.id : -1)}
+                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded text-xs cursor-pointer">
+                      삭제
+                    </button>
                   </td>
                 </tr>
               ))}
