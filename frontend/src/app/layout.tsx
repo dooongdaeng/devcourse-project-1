ba2130b48503c90cee2a5b4f23f3431047750faf
eunchan <taegwon02@gmail.com>
2025-07-18 17:40:07 +0900

    feat: layout 컴포넌트 분리 (#316)
    * feat: 서버 컴포넌트와 클라이언트 컴포넌트 분리
    
    * style: decription 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,8 @@
-"use client";
-
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { ProductProvider } from '@/context/ProductContext';
-import { UserProvider, useUser } from '@/context/UserContext';
 import React from 'react';
+import ClientLayout from "./ClientLayout"
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -18,116 +14,23 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
-function AppContent({ children }: { children: React.ReactNode }) {
-    const { user: currentUser, logout } = useUser();
-    const router = useRouter();
-
-    const handleLogout = () => {
-        logout();
-        alert('로그아웃 되었습니다.');
-        router.push('/login');
-    };
-
-    return (
-        <html lang="en">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased}`}
-        >
-        <header className="bg-gray-800 text-white p-4 shadow-md">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link href="/" className="text-2xl font-bold">
-                    Grids & Circles
-                </Link>
-                <nav>
-                    <ul className="flex space-x-4">
-                        <li>
-                            <Link href="/products" className="hover:text-gray-300">
-                                제품소개
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/order" className="hover:text-gray-300">
-                                주문
-                            </Link>
-                        </li>
-                        {currentUser && (
-                            <>
-                                <li>
-                                    <Link href="/orderHistory" className="hover:text-gray-300">
-                                        주문내역
-                                    </Link>
-                                </li>
-                                {currentUser.role !== 'ROLE_ADMIN' && (
-                                    <li>
-                                        <Link href="/account/client" className="hover:text-gray-300">
-                                            회원정보
-                                        </Link>
-                                    </li>
-                                )}
-                                {currentUser.role === 'ROLE_ADMIN' && (
-                                    <>
-                                        <li>
-                                            <Link href="/account/admin/accountManagement" className="hover:text-gray-300">
-                                                회원정보관리
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/account/admin/productManagement" className="hover:text-gray-300">
-                                                상품관리
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/account/admin/orderManagement" className="hover:text-gray-300">
-                                                주문관리
-                                            </Link>
-                                        </li>
-                                    </>
-                                )}
-                            </>
-                        )}
-                        <li>
-                            {currentUser ? (
-                                <button onClick={handleLogout} className="hover:text-gray-300 cursor-pointer">
-                                    로그아웃
-                                </button>
-                            ) : (
-                                <Link href="/login" className="hover:text-gray-300">
-                                    로그인
-                                </Link>
-                            )}
-                        </li>
-                        {!currentUser && (
-                            <li>
-                                <Link href="/signup" className="hover:text-gray-300">
-                                    회원가입
-                                </Link>
-                            </li>
-                        )}
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <main>{children}</main>
-        <footer></footer>
-        </body>
-        </html>
-    );
-}
+export const metadata: Metadata = {
+    title: "Grids & Circles",
+    description: "Grids & Circles는 다양한 커피 메뉴를 손쉽게 주문하고 즐길 수 있는 스마트한 커피 주문 플랫폼입니다. 오늘도 당신의 커피 한 잔을 책임질게요!",
+};
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased}`}>
-        <UserProvider>
-            <ProductProvider>
-                <AppContent>{children}</AppContent>
-            </ProductProvider>
-        </UserProvider>
-        </body>
+        <html lang="ko">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased}`}
+            >
+                <ClientLayout>{children}</ClientLayout>
+            </body>
         </html>
     );
 }
